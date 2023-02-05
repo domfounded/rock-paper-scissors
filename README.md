defa4a90da0ea997b5c407235aa7a1c9a79be614
Dominic <castillodom07@gmail.com>
2023-02-04 17:11:44 -0800

    Create pseudocode

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # rock-paper-scissors
+Third Odin Project. First JavaScript project.
+
+Simple rock paper scissors game played through the browser console only. GUI to come in the future.
+
+Been taking a lot of breaks between studying. Need to put more time and focus into TOP.
---
+++ app.js
@@ -0,0 +1,36 @@
+function getComputerChoice() {
+    // return random choice from computer between rock, paper, scissors
+}
+
+function getPlayerChoice() {
+    // ask player to pick betwen rock, paper, scissors
+        // use prompt()
+    // set whatever player picks to a number
+    // return player choice
+}
+
+function playRound(playerSelection, computerSelection) {
+    /*
+    let playerSelection equal getPlayerChoice
+    let computerSelection equal getComputerChoice
+
+    figure out winner
+        if playerSelection = computerSelection, tie
+        else if (player is rock, comp is scissor) or (player paper, comp rock) or (player scissor, comp paper)
+            player wins
+        else
+            player lose
+
+    return string to declare winner
+    */
+}
+
+function game() {
+    /* 
+    play five rounds of the game
+        for loop from 0 to 5
+            console.log() results of round
+        end loop
+        console.log() winner of game
+    */
+}
